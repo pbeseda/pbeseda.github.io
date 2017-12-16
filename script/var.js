124ebc7bf323281493c198ce3e649f2820f7de58
Patrick Beseda <patrick.beseda@gmail.com>
2017-12-16 16:10:26 -0700

    new site

---
+++ script/var.js
@@ -0,0 +1,97 @@
+// All the variable for the app
+// Their name are explicit
+
+var username = "pbeseda"
+var clientid = "be212a58528168962a39c64052c1d88e";
+var redirecturi = "http://localhost:8002/callback.html";
+var locations = [
+  "12792957"
+];
+var images = [
+  "bg.jpg"
+];
+var searchs = [
+  ["!g", "https://www.google.ch/#q="],
+  ["!y", "https://www.youtube.com/results?search_query="],
+  ["!w", "https://en.wikipedia.org/w/index.php?search="]
+];
+var favorites = [
+  [ "Work",
+    [
+      ["inbox" , "https://inbox.google.com/?pli=1", "in"],
+      ["drive" , "https://drive.google.com/drive/my-drive", "dr"],
+      ["calendar" , "https://calendar.google.com/calendar/r", "cal"],
+      ["photos" , "https://photos.google.com/", "ph"],
+      ["maps" , "https://www.google.com/maps?hl=en&tab=ql", "m"],
+      ["harvest" , "https://fndry.harvestapp.com/time", "hv"],
+      ["trello" , "https://trello.com/", "tr"]
+    ]
+  ],
+  [ "Social",
+    [
+      ["instagram" , "https://www.instagram.com/", "ig"],
+      ["youtube" , "https://www.youtube.com/", "yt"],
+      ["facebook" , "https://www.facebook.com/" , "fb"],
+      ["twitter" , "https://twitter.com/", "twi"]
+    ]
+  ],
+  [ "make",
+    [
+      ["formlabs dashboard" , "https://formlabs.com/dashboard/", "fl"],
+      ["formlabs forum" , "https://forum.formlabs.com/", "flf"],
+      ["thingiverse" , "http://www.thingiverse.com/" , "th"],
+      ["fusion forum" , "https://forums.autodesk.com/t5/fusion-360/ct-p/1234", "fus"]
+    ]
+  ],
+  [ "resources",
+    [
+      ["thepiratebay", "http://thepiratebay.se", "tpb"],
+      ["creative market", "http://thepiratebay.se", "cr"],
+      ["unit conversion", "http://thepiratebay.se", "con"],
+      ["codrops", "https://tympanus.net/codrops/", "cod"],
+      ["github" , "https://github.com/", "gh"],
+      ["lastpass" , "https://lastpass.com/?&ac=1&lpnorefresh=1&fromwebsite=1&newvault=1&nk=1", "lp"],
+      ["dropbox", "https://www.dropbox.com/home", "d"]
+    ]
+  ],
+  [ "FNDRY",
+    [
+      ["FNDRYFNDRY.com", "https://fndryfndry.com/", "f"],
+      ["FNDRY Hosting Login", "https://fndryfndry.com/", "f"],
+      ["Shopify login", "https://topoproject.myshopify.com/admin/auth/login", "mh"],
+      ["Xero", "https://login.xero.com/", "x"],
+      ["Spark", "https://sparkbusiness3.capitalone.com/app.html#/accountSummary/", "sp"],
+      ["stripe", "https://dashboard.stripe.com/login?redirect=%2Fdashboard", "st"],
+      ["printful", "https://www.printful.com/auth/login", "ph"]
+    ]
+  ],
+  [ "4chan",
+    [
+      ["slack", "https://www.reddit.com/", "fp"],
+      ["pinterest", "https://www.pinterest.com/", "p"],
+      ["reddit", "https://www.reddit.com/", "r"],
+    ]
+  ],
+  [ "Financial",
+    [
+      ["Personal Capital" , "https://www.mint.com/", "pc"],
+      ["CU of CO" , "https://www.cuofco.org/", "cu"],
+      ["Simple" , "https://bank.simple.com/signin", "sim"],
+      ["Paypal" , "https://www.paypal.com/signin", "pay"],
+      ["Nelnet" , "https://www.nelnet.com/welcome", "nel"],
+      ["Acorns" , "https://app.acorns.com/dashboard/account", "ac"],
+      ["Betterment" , "https://wwws.betterment.com/app/summary", "be"]
+    ]
+  ],
+  [ "Others",
+    [
+      ["twig", "https://twigcustombuilders.com/", "tw"],
+      ["airforums", "https://www.airforums.com/", "air"]
+    ]
+  ]
+];
+var feeds = [
+  ["20min Hi-Tech","http://www.20min.ch/rss/rss.tmpl?type=channel&get=20&lang=ro"],
+  ["Internet is Beautiful", "https://www.reddit.com/r/InternetIsBeautiful/.rss"],
+  ["Listen to this", "https://www.reddit.com/r/listentothis/.rss"]
+];
